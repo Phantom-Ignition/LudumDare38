5561cd9db42ed783857754f04ff91064f1e17711
Rafael Almeida <rafaelalmeidatk@gmail.com>
2017-04-21 21:39:58 -0300

    Particles test

--- LurumDare38/Scenes/SceneMap.cs
+++ LurumDare38/Scenes/SceneMap.cs
@@ -7,6 +7,11 @@ using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
 using LurumDare38.Characters;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Particles;
+using MonoGame.Extended.Particles.Profiles;
+using MonoGame.Extended.Particles.Modifiers;
+using System.Collections.Generic;
+using MonoGame.Extended.TextureAtlases;
 
 namespace LurumDare38.Scenes
 {
@@ -28,9 +33,19 @@ namespace LurumDare38.Scenes
         public Player Player { get { return _player; } }
 
         //--------------------------------------------------
+        // Particle Effects
+
+        private List<ParticleEffect> _particleEffects;
+
+        //--------------------------------------------------
         // Random
 
         private Random _rand;
+
+        //--------------------------------------------------
+        // Test particles
+
+        private ParticleEffect _particleTest;
 
         //----------------------//------------------------//
 
@@ -41,7 +56,9 @@ namespace LurumDare38.Scenes
 
         public override void LoadContent()
         {
+
             base.LoadContent();
+
 
             var viewportSize = SceneManager.Instance.VirtualSize;
             _camera = new Camera2D(SceneManager.Instance.ViewportAdapter);
@@ -49,11 +66,47 @@ namespace LurumDare38.Scenes
             // Player init
             _player = new Player(ImageManager.loadCharacter("Player"));
 
+            // Particles init
+            var particleTexture = new Texture2D(SceneManager.Instance.GraphicsDevice, 1, 1);
+            particleTexture.SetData(new[] { Color.White });
+            ParticlesInit(new TextureRegion2D(particleTexture));
+
             // Random init
             _rand = new Random();
 
             // Load the map
             LoadMap(MapManager.FirstMap);
+        }
+
+        private void ParticlesInit(TextureRegion2D textureRegion)
+        {
+            _particleTest = new ParticleEffect()
+            {
+                Name = "Test",
+                Emitters = new[]
+                {
+                    new ParticleEmitter(500, TimeSpan.FromSeconds(1.5), Profile.Spray(Axis.Up, (float)Math.PI))
+                    {
+                        TextureRegion = textureRegion,
+                        Parameters = new ParticleReleaseParameters()
+                        {
+                            Speed = new RangeF(30f, 60f),
+                            Quantity = 10,
+                            Rotation = new RangeF(-1f, 1f),
+                            Scale = new RangeF(2.0f, 4.5f),
+                            Color = new HslColor(186, 0.13f, 0.96f)
+                        },
+                        Modifiers = new IModifier[]
+                        {
+                            new LinearGravityModifier { Direction = Axis.Down, Strength = 150f },
+                            new RotationModifier { RotationRate = 2.0f },
+                            new OpacityFastFadeModifier()
+                        }
+                    }
+                }
+            };
+            _particleEffects = new List<ParticleEffect>();
+            _particleEffects.Add(_particleTest);
         }
 
         private void LoadMap(int mapId)
@@ -81,13 +134,21 @@ namespace LurumDare38.Scenes
 
         public override void Update(GameTime gameTime)
         {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             _player.Update(gameTime);
             UpdateCamera();
+            UpdateParticles(deltaTime);
             base.Update(gameTime);
 
             if (InputManager.Instace.KeyPressed(Keys.M))
             {
                 MapManager.Instance.LoadMapWithTransition(2, MapLoadedFromTransition);
+            }
+
+            if (InputManager.Instace.KeyPressed(Keys.P))
+            {
+                _particleTest.Trigger(SceneManager.Instance.VirtualSize / 2);
             }
 
             DebugValues["Delta Time"] = gameTime.ElapsedGameTime.TotalMilliseconds.ToString();
@@ -107,6 +168,11 @@ namespace LurumDare38.Scenes
             _camera.Position = new Vector2(x, y);
         }
 
+        private void UpdateParticles(float deltaTime)
+        {
+            _particleEffects.ForEach(particle => particle.Update(deltaTime));
+        }
+
         public override void Draw(SpriteBatch spriteBatch, ViewportAdapter viewportAdapter)
         {
             base.Draw(spriteBatch, viewportAdapter);
@@ -120,6 +186,9 @@ namespace LurumDare38.Scenes
             // Draw the player
             _player.DrawCharacter(spriteBatch);
             if (debugMode) _player.DrawColliderBox(spriteBatch);
+
+            // Draw the particles
+            _particleEffects.ForEach(particle => spriteBatch.Draw(particle));
 
             spriteBatch.End();
         }
